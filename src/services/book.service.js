d1e1ee4ad3aa18e94b0d11f6f74a98580a5cd78d
Ahmed Hussein <ahmedhussiien@gmail.com>
2024-02-02 23:11:42 +0200

    feat: add searching by author, title and isbn to books

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import _ from 'lodash';
 const { assign, omit } = _;
 
@@ -6,15 +7,33 @@ import NotFoundError from '../utils/exceptions/notFoundError.js';
 import BadRequestError from '../utils/exceptions/badRequestError.js';
 import { getPaginationInfo, getPagingData } from '../utils/paginationHelper.js';
 
-const { Book } = db;
+const { Book, BookAuthor } = db;
 
 async function findAll(query) {
   // search with params
   const { title, authorName, ISBN } = query;
+  const whereStatement = {};
+  const include = [];
+
+  if (title) whereStatement.title = { [Op.like]: '%' + title + '%' };
+  if (ISBN) whereStatement.ISBN = ISBN;
+
+  if (authorName)
+    include.push({
+      model: BookAuthor,
+      where: {
+        name: { [Op.like]: '%' + authorName + '%' },
+      },
+    });
 
   // set pagination
   const { limit, offset, page } = getPaginationInfo(query.page, query.limit);
-  const data = await Book.findAndCountAll({ limit, offset });
+  const data = await Book.findAndCountAll({
+    where: whereStatement,
+    include,
+    limit,
+    offset,
+  });
 
   // format response
   const result = getPagingData(data, page, limit);
