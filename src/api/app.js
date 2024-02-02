e3df930b9d85d9b099f9e9cebcfbd1ad35cff510
Ahmed Hussein <ahmedhussiien@gmail.com>
2024-02-02 03:54:24 +0200

    feat: use helmet and cors for api security headers

--- src/api/app.js
+++ src/api/app.js
@@ -1,17 +1,34 @@
 import express from 'express';
 import 'express-async-errors';
 
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+
 import healthRoute from './routes/health.route.js';
 import v1Routes from './routes/v1/index.js';
 import errorController from './middlewares/error.controller.js';
 
 const app = express();
 
+// disable `X-Powered-By` header that reveals information about the server
+app.disable('x-powered-by');
+
+// set security HTTP headers
+app.use(helmet());
+
 // parse json request body
 app.use(express.json());
 
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// log requests
+app.use(morgan('dev'));
 
 // health route
 app.use('/health', healthRoute);
